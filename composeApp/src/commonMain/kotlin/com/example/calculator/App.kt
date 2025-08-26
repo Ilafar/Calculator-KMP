aa9fada92d9b99f86b330f2a285ffecf84d0e085
Ilafer <ezizliilafer@gmail.com>
2025-08-26 03:25:38 -0700

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/example/calculator/App.kt
@@ -0,0 +1,25 @@
+package com.example.calculator
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Anar geydi",
+                color = Color.White,
+                fontSize = 32.sp
+            )
+        }
+    }
+}
